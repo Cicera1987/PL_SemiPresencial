7285183e26f9976ead7b68cec7e7595a4ce46a6e
Cicera Ribeiro <maccicera@gmail.com>
2023-04-02 18:15:46 -0300

    Update Animal.java

--- src/Animal/Animal.java
+++ src/Animal/Animal.java
@@ -7,4 +7,28 @@ Crie também um método chamado "dormir" que imprima uma mensagem indicando que 
 
 
 public class Animal {
+  // atributos da classe
+    public String name;
+    public String species;
+    public int age;
+
+    // metodos da classe emitirSom() e dormir()
+    public void emitirSom(){
+        System.out.println(name + "está emitindo um som");
+    }
+    public  void  dormir(){
+        System.out.println(name + "esta dormindo");
+    }
+
+    public static void main(String[] args) {
+        ///Imprimindo resultado
+        Animal aminal_01 = new Animal();
+        aminal_01.name = "Gato ";
+        aminal_01.species = "Felino";
+        aminal_01.age = 3;
+
+        aminal_01.emitirSom();
+        aminal_01.dormir();
+    }
+  
 }
