9f1d402e176c43af288d3046c97a9ddd2eb02071
Cicera Ribeiro <maccicera@gmail.com>
2023-04-02 18:17:04 -0300

    Update Circulo.java

--- src/Circulo/Circulo.java
+++ src/Circulo/Circulo.java
@@ -13,4 +13,17 @@ public class Circulo {
     public  Circulo(double raio){this.raio = raio;}
     public double calcularArea(){return Math.PI * raio * raio;}
     public  double calcularPerimetro(){return  2 * Math.PI * raio;}
+    
+     public static void main(String[] args) {
+        //Criando Objeto da classe
+        Circulo circulo = new Circulo(4);
+
+        // chamando o método  CalcularArea() e CalcularPerímetro do objeto
+        double area = circulo.CalcularArea();
+        double perimetro = circulo.CalcularPerimetro();
+
+        // imprimindo resultado
+        System.out.println(" A área do Circulo é: " + area);
+        System.out.println(" o perímetro do Circulo é: " +perimetro);
+    }
 }
